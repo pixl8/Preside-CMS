7e2fc090b47c55e6b8b8809e975f17578252bfd4
Dominic Watson <watson.dominic@gmail.com>
2014-06-30 15:58:05 +0100

    Allowing the preside bootbox modal widget to work with URLs as well as local html target references
    [skip-ci]

--- system/assets/js/admin/presidecore/preside.bootbox.modal.js
+++ system/assets/js/admin/presidecore/preside.bootbox.modal.js
@@ -13,47 +13,63 @@
 		e.preventDefault();
 
 		if ( typeof modalConfig === "undefined" ) {
-			modalConfig = initModalConfig( $modalLink );
+			initModalConfig( $modalLink, function( modalConfig ){
+				openModal( modalConfig );
+			} );
+		} else {
+			openModal( modalConfig );
 		}
-
-		openModal( modalConfig );
 	}
 
 	openModal = function( config ){
 		presideBootbox.dialog( config );
 	};
 
-	initModalConfig = function( $modalLink ){
+	initModalConfig = function( $modalLink, callback ){
 		var buttonList = ( $modalLink.data( "buttons" ) || "ok cancel" ).split( " " )
-		  , $target    = $( $modalLink.get(0).hash )
-		  , config, i;
+		  , config, i, setupConfig;
 
-		config = {
-			  title     : $modalLink.data( "title" ) || $modalLink.attr( "title" )
-			, message   : $target.html()
-			, className : $modalLink.data( "modalClass" )
-			, buttons   : {}
+		setupConfig = function( content ){
+			config = {
+				  title     : $modalLink.data( "title" ) || $modalLink.attr( "title" )
+				, message   : content
+				, className : $modalLink.data( "modalClass" )
+				, buttons   : {}
+			};
+
+			for( i=0; i < buttonList.length; i++ ){
+				if ( buttonList[i] === "cancel" ) {
+					config.buttons.cancel = {
+						label     : i18n.translateResource( "cms:cancel.btn" ),
+						className : "btn-default",
+						callback  : function(){ btnCallBack( $modalLink, "cancel" ); }
+					};
+				} else if ( buttonList[i] === "ok" ) {
+					config.buttons.ok = {
+						label     : i18n.translateResource( "cms:ok.btn" ),
+						className : "btn-primary",
+						callback  : function(){ btnCallBack( $modalLink, "ok" ); }
+					};
+				}
+			}
+
+			$modalLink.data( "bootboxModalConfig", config );
+
+			callback( config );
 		};
 
-		for( i=0; i < buttonList.length; i++ ){
-			if ( buttonList[i] === "cancel" ) {
-				config.buttons.cancel = {
-					label     : i18n.translateResource( "cms:cancel.btn" ),
-					className : "btn-default",
-					callback  : function(){ btnCallBack( $modalLink, "cancel" ); }
-				};
-			} else if ( buttonList[i] === "ok" ) {
-				config.buttons.ok = {
-					label     : i18n.translateResource( "cms:ok.btn" ),
-					className : "btn-primary",
-					callback  : function(){ btnCallBack( $modalLink, "ok" ); }
-				};
-			}
+		if ( $modalLink.get(0).hash.length ) {
+			setupConfig( $( modalLink.get(0).hash ).html() );
+		} else {
+			$.ajax( {
+				  method  : "GET"
+				, url     : $modalLink.attr( 'href' )
+				, cache   : false
+				, success : function( content ){ setupConfig( content ); }
+			} );
 		}
 
-		$modalLink.data( "bootboxModalConfig", config );
 
-		return config;
 	};
 
 	btnCallBack = function( $modalLink, action ){
