11310139ab2847ad7379733d6ec39ac7cca37b3d
Dominic Watson <watson.dominic@gmail.com>
2014-07-02 10:37:17 +0100

    OP-179 Refactoring our preside modal js so that it can be used manually with callback options from other js
    [skip-ci]

--- system/assets/js/admin/presidecore/preside.bootbox.modal.js
+++ system/assets/js/admin/presidecore/preside.bootbox.modal.js
@@ -1,81 +1,85 @@
 ( function( $ ){
 
-	var openModal, registerEventHandlers, launchHandler, btnCallBack;
+	$.fn.presideBootboxModal = function( modalOptions ){
+		return this.each( function(){
+			var $modalLink = $( this )
+			  , openModal, registerEventHandlers, launchHandler, btnCallBack;
 
-	registerEventHandlers = function(){
-		$( "body" ).on( "click", '[data-toggle="bootbox-modal"]', launchHandler );
-	};
+			launchHandler = function( e ){
+				var modalConfig = $modalLink.data( "bootboxModalConfig" );
 
-	launchHandler = function( e ){
-		var $modalLink  = $( this )
-		  , modalConfig = $modalLink.data( "bootboxModalConfig" );
+				e.preventDefault();
 
-		e.preventDefault();
-
-		if ( typeof modalConfig === "undefined" ) {
-			initModalConfig( $modalLink, function( modalConfig ){
-				openModal( modalConfig );
-			} );
-		} else {
-			openModal( modalConfig );
-		}
-	}
-
-	openModal = function( config ){
-		presideBootbox.dialog( config );
-	};
-
-	initModalConfig = function( $modalLink, callback ){
-		var buttonList = ( $modalLink.data( "buttons" ) || "ok cancel" ).split( " " )
-		  , config, i, setupConfig;
-
-		setupConfig = function( content ){
-			config = {
-				  title     : $modalLink.data( "title" ) || $modalLink.attr( "title" )
-				, message   : content
-				, className : $modalLink.data( "modalClass" )
-				, buttons   : {}
-			};
-
-			for( i=0; i < buttonList.length; i++ ){
-				if ( buttonList[i] === "cancel" ) {
-					config.buttons.cancel = {
-						label     : i18n.translateResource( "cms:cancel.btn" ),
-						className : "btn-default",
-						callback  : function(){ btnCallBack( $modalLink, "cancel" ); }
-					};
-				} else if ( buttonList[i] === "ok" ) {
-					config.buttons.ok = {
-						label     : i18n.translateResource( "cms:ok.btn" ),
-						className : "btn-primary",
-						callback  : function(){ btnCallBack( $modalLink, "ok" ); }
-					};
+				if ( typeof modalConfig === "undefined" ) {
+					initModalConfig( function( modalConfig ){
+						openModal( modalConfig );
+					} );
+				} else {
+					openModal( modalConfig );
 				}
 			}
 
-			$modalLink.data( "bootboxModalConfig", config );
+			openModal = function( config ){
+				var modal = presideBootbox.dialog( config );
+				if ( typeof modalOptions === "object" && typeof modalOptions.onShow === "function" ) {
+					modalOptions.onShow( modal );
+				}
+			};
 
-			callback( config );
-		};
+			initModalConfig = function( callback ){
+				var buttonList = ( $modalLink.data( "buttons" ) || "ok cancel" ).split( " " )
+				  , config, i, setupConfig;
 
-		if ( $modalLink.get(0).hash.length ) {
-			setupConfig( $( $modalLink.get(0).hash ).html() );
-		} else {
-			$.ajax( {
-				  method  : "GET"
-				, url     : $modalLink.attr( 'href' )
-				, cache   : false
-				, success : function( content ){ setupConfig( content ); }
-			} );
-		}
+				setupConfig = function( content ){
+					config = {
+						  title     : $modalLink.data( "title" ) || $modalLink.attr( "title" )
+						, message   : content
+						, className : $modalLink.data( "modalClass" )
+						, buttons   : {}
+					};
+
+					for( i=0; i < buttonList.length; i++ ){
+						if ( buttonList[i] === "cancel" ) {
+							config.buttons.cancel = {
+								label     : '<i class="fa fa-reply"></i> ' + i18n.translateResource( "cms:cancel.btn" ),
+								className : "btn-default",
+								callback  : function(){ btnCallBack( $modalLink, "cancel" ); }
+							};
+						} else if ( buttonList[i] === "ok" ) {
+							config.buttons.ok = {
+								label     : '<i class="fa fa-check"></i> ' + i18n.translateResource( "cms:ok.btn" ),
+								className : "btn-primary",
+								callback  : function(){ btnCallBack( $modalLink, "ok" ); }
+							};
+						}
+					}
+
+					$modalLink.data( "bootboxModalConfig", config );
+
+					callback( config );
+				};
+
+				if ( $modalLink.get(0).hash.length ) {
+					setupConfig( $( $modalLink.get(0).hash ).html() );
+				} else {
+					$.ajax( {
+						  method  : "GET"
+						, url     : $modalLink.attr( 'href' )
+						, cache   : false
+						, success : function( content ){ setupConfig( content ); }
+					} );
+				}
 
 
+			};
+
+			btnCallBack = function( $modalLink, action ){
+				$modalLink.trigger( "bootboxModal" + action );
+			};
+
+			$modalLink.on( "click", launchHandler );
+		} );
 	};
 
-	btnCallBack = function( $modalLink, action ){
-		$modalLink.trigger( "bootboxModal" + action );
-	};
-
-	registerEventHandlers();
-
+	$( '[data-toggle="bootbox-modal"]' ).presideBootboxModal( {} );
 } )( presideJQuery );
