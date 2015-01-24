6703e46dc19b089c74e0ecf3b50e37a4cd13ccf0
Dominic Watson <watson.dominic@gmail.com>
2015-01-24 16:58:14 +0000

    Putting a load of that RTD js back - leaving just the piece that deals with toggling the versioning menu in place

--- support/docs/source/_themes/pixl8_rtd_theme/static/js/theme.js
+++ support/docs/source/_themes/pixl8_rtd_theme/static/js/theme.js
@@ -1,10 +1,53 @@
 $( document ).ready(function() {
+    // Shift nav in mobile when clicking the menu.
+    $(document).on('click', "[data-toggle='wy-nav-top']", function() {
+      $("[data-toggle='wy-nav-shift']").toggleClass("shift");
+      $("[data-toggle='rst-versions']").toggleClass("shift");
+    });
+    // Close menu when you click a link.
+    $(document).on('click', ".wy-menu-vertical .current ul li a", function() {
+      $("[data-toggle='wy-nav-shift']").removeClass("shift");
+      $("[data-toggle='rst-versions']").toggleClass("shift");
+    });
+    // Make tables responsive
+    $("table.docutils:not(.field-list)").wrap("<div class='wy-table-responsive'></div>");
+
     // crazy TOC hack
     var $pageToc   = $( '#page-toc' )
       , $pageTitle = $pageToc.prev().find( "h1:first" );
 
     $pageToc.insertAfter( $pageTitle );
 });
+
+window.SphinxRtdTheme = (function (jquery) {
+    var stickyNav = (function () {
+        var navBar,
+            win,
+            stickyNavCssClass = 'stickynav',
+            applyStickNav = function () {
+                if (navBar.height() <= win.height()) {
+                    navBar.addClass(stickyNavCssClass);
+                } else {
+                    navBar.removeClass(stickyNavCssClass);
+                }
+            },
+            enable = function () {
+                applyStickNav();
+                win.on('resize', applyStickNav);
+            },
+            init = function () {
+                navBar = jquery('nav.wy-nav-side:first');
+                win    = jquery(window);
+            };
+        jquery(init);
+        return {
+            enable : enable
+        };
+    }());
+    return {
+        StickyNav : stickyNav
+    };
+}($));
 
 ( function( $ ){
 
