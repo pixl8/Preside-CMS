3b978a8f620a8147eea7fcbea6cb8216b4d20c55
Dominic Watson <watson.dominic@gmail.com>
2015-04-15 14:21:17 +0100

    Including 'typeahead' jquery plugin + begninning to style it for asset search

--- system/assets/Gruntfile.js
+++ system/assets/Gruntfile.js
@@ -82,7 +82,7 @@ module.exports = function( grunt ) {
 					  	"js/admin/lib/plugins/jquery.dataTables.js", // must come first
 					  	"js/admin/lib/plugins/*.js"
 					  ]
-					, dest : "js/admin/lib/plugins-1.0.006.min.js"
+					, dest : "js/admin/lib/plugins-1.0.007.min.js"
 				},{
 					  src  : ["js/admin/lib/ace/ace.js", "js/admin/lib/ace/ace-elements.js"]
 					, dest : "js/admin/lib/ace-1.0.0.min.js"
