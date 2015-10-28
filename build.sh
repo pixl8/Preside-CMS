c1f3d5b1590b33ae4e2a9b42e05b98ed57ba34f9
Dominic Watson <watson.dominic@gmail.com>
2015-10-28 18:43:45 +0000

    PRESIDECMS-194 making the new build system more self documenting + skipping docs + builds on irrelevant branches in travis

--- build.sh
+++ build.sh
@@ -3,33 +3,60 @@
 cd `dirname $0`
 CWD="`pwd`"
 
+echo "";
+echo "--------------------------------------------------------";
 echo "Welcome to the PresideCMS Test runner and build suite :)";
 echo "--------------------------------------------------------";
-echo ".";
-echo "Building dependencies via box.json";
-echo ".";
-echo ".";
+echo "";
+echo "This script will install dependencies, run tests, and conditionally build docs and package preside into a ZIP file.";
+echo "Each of these operations can be run independantly with the following commands:";
+echo "";
+echo "box install                      - installs dependencies";
+echo "./test.sh                        - runs tests";
+echo "./support/docs/build.sh          - builds documentation";
+echo "ant -f ./support/build/build.xml - packages PresideCMS into a zip file";
+echo "";
+echo "The script has dependencies on CommandBox, ant + an accessible database for running the test suite.";
+echo "";
+echo "-------------------------------------------------------";
+echo "";
+echo "Installing dependencies via box.json...";
+echo "";
 
 box install --force
 
-echo ".";
-echo ".";
+echo "";
 echo "Running tests (please be patient, expect this to take several minutes)...";
-echo ".";
-echo ".";
+echo "";
 
 ./test.sh
 
-echo ".";
-echo ".";
+if  [[ $TRAVIS_PULL_REQUEST == 'true' ]] ; then
+	echo "Finished. (not packaging up docs or source due to running in a pull request)."
+	exit 0;
+fi
 
-./support/docs/build.sh
+if [[ $TRAVIS_BRANCH == 'stable' ]] ; then
+	./support/docs/build.sh
 
-echo ".";
-echo ".";
+	echo "";
+	echo "";
+else
+	echo "Skipping docs build, not on stable branch in a travis build. To build the docs yourself, run ./support/docs/build.sh"
+fi
 
-echo "Packaging up as zip file";
-echo ".";
-echo ".";
+if [[ $TRAVIS_BRANCH == 'stable' ]] || [[ $TRAVIS_BRANCH == 'release-*' ]] ; then
+	echo "Packaging up as zip file...";
+	echo "";
+	echo "";
 
-ant -f support/build/build.xml
+	ant -f support/build/build.xml
+else
+	echo "Skipping packaging, not on stable or release branch in a travis build. To package preside yourself, run ant -f ./support/build/build.xml"
+fi
+
+echo "";
+echo "All done :)";
+echo "";
+
+exit 0;
