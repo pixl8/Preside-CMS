63e63c6eb58996eb5bb7cd9ad362bf643d2b3c94
CF Mitrah <j_saravanamuthu@yahoo.com>
2016-07-27 12:09:13 +0530

    PRESIDECMS-453: Fixed item selection issue on assetManager and dataManager listing tables.

--- system/assets/js/admin/specific/assetmanager/index/assetBrowser.js
+++ system/assets/js/admin/specific/assetmanager/index/assetBrowser.js
@@ -59,14 +59,31 @@
 			var $allCBoxes = $listingTable.find( 'tr > td:first-child input:checkbox' );
 
 			$allCBoxes.each( function(){
-				this.checked = $selectAllCBox.is( ':checked' );
-				$(this).closest('tr').toggleClass('selected');
+				if( $selectAllCBox.is( ':checked' ) ) {
+					this.checked = true;
+					$(this).closest( 'tr' ).addClass( 'selected' );
+				}
+				else {
+					this.checked = false;
+					$(this).closest( 'tr' ).removeClass( 'selected' );
+				}
 			});
 		});
 
+		$listingTable.on( 'click', 'tbody :checkbox', function(){
+			var $cbox = $( this );
+			$cbox.closest( 'tr' ).toggleClass( 'selected', $cbox.is( ':checked' ) );
+		});
+		
 		$multiActionBtns.data( 'hidden', true );
 		$listingTable.on( "click", "th input:checkbox,tbody tr > td:first-child input:checkbox", function( e ){
 			var anyBoxesTicked = $listingTable.find( 'tr > td:first-child input:checkbox:checked' ).length;
+
+			if( anyBoxesTicked == $listingTable.find( "td input:checkbox" ).length ) {
+				$selectAllCBox.prop( 'checked', true );
+			} else {
+				$selectAllCBox.prop( 'checked', false );
+			}
 
 			enabledContextHotkeys( !anyBoxesTicked );
 
--- system/assets/js/admin/specific/datamanager/object/listingTable.js
+++ system/assets/js/admin/specific/datamanager/object/listingTable.js
@@ -127,14 +127,26 @@
 			var $allCBoxes = $listingTable.find( 'tr > td:first-child input:checkbox' );
 
 			$allCBoxes.each( function(){
-				this.checked = $selectAllCBox.is( ':checked' );
-				$(this).closest('tr').toggleClass('selected');
+				if( $selectAllCBox.is( ':checked' ) ) {
+					this.checked = true;
+					$(this).closest( 'tr' ).addClass( 'selected' );
+				}
+				else {
+					this.checked = false;
+					$(this).closest( 'tr' ).removeClass( 'selected' );
+				}
 			});
 		});
 
 		$multiActionBtns.data( 'hidden', true );
 		$listingTable.on( "click", "th input:checkbox,tbody tr > td:first-child input:checkbox", function( e ){
 			var anyBoxesTicked = $listingTable.find( 'tr > td:first-child input:checkbox:checked' ).length;
+
+			if(anyBoxesTicked == $listingTable.find( "td input:checkbox" ).length) {
+				$selectAllCBox.prop( 'checked', true );
+			} else {
+				$selectAllCBox.prop( 'checked', false );
+			}
 
 			enabledContextHotkeys( !anyBoxesTicked );
 
@@ -173,22 +185,9 @@
 	};
 
 	setupTableRowFocusBehaviour = function(){
-		var focusSelector = useMultiActions ? 'tbody :checkbox' : 'tbody tr a';
-
-		$listingTable.on( 'focus', focusSelector, function(){
-			$( this ).closest( 'tr' ).addClass( 'focus' );
-		} );
-		$listingTable.on( 'blur', focusSelector, function(){
-			$( this ).closest( 'tr' ).removeClass( 'focus' );
-		} );
-
 		$listingTable.on( 'click', 'tbody :checkbox', function(){
 			var $cbox = $( this );
 			$cbox.closest( 'tr' ).toggleClass( 'selected', $cbox.is( ':checked' ) );
-		} );
-
-		$listingTable.on( 'keydown', 'tr.focus', 'return', function(){
-			$( this ).click();
 		} );
 	};
 
