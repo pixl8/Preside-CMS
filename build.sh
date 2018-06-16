4779380e5c8cc75f9087ec1708bbd713856ca9bb
Dominic Watson <watson.dominic@gmail.com>
2018-06-16 09:24:24 +0100

    Temporarily disable tests while we play with build...

--- build.sh
+++ build.sh
@@ -37,12 +37,12 @@ npm install || exit 1;
 grunt all || exit 1;
 cd ../../;
 
-echo "";
-echo "Running tests (please be patient, expect this to take several minutes)...";
-echo "";
+# echo "";
+# echo "Running tests (please be patient, expect this to take several minutes)...";
+# echo "";
 
 
-./test.sh || exit 1;
+# ./test.sh || exit 1;
 
 if [[ $TRAVIS_JDK_VERSION == 'openjdk7' ]] ; then
 	echo "Finished (only run tests on JDK 7, builds performed in the JDK8 environment)."
