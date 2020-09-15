7e774b93bd99bc4b3ac522f8544e56a4851198ce
Teon Ooi <teon@pixl8.co.uk>
2020-09-15 19:11:31 +0800

    PRESIDECMS-1956 Update dialog with optional confirmation text.

--- system/assets/js/admin/presidecore/preside.confirmation.prompts.js
+++ system/assets/js/admin/presidecore/preside.confirmation.prompts.js
@@ -1,12 +1,13 @@
 ( function( $ ){
 
 	$("body").on( "click", ".confirmation-prompt", function( e ) {
-		var $link = $( this )
-		  , title;
-
 		e.preventDefault();
 
-		if ( !$link.data( "confirmationPrompt" ) ) {
+		var $link = $( this )
+		  , title = ""
+		;
+
+		if( !$link.data( "confirmationPrompt" ) ) {
 			title = $link.data( "title" ) || $link.attr("title");
 			title = title.charAt(0).toLowerCase() + title.slice(1);
 			var hasChildren = $link.attr( "data-has-children" );
@@ -17,15 +18,60 @@
 			}
 		}
 
-		presideBootbox.confirm( $link.data( "confirmationPrompt" ), function( confirmed ) {
-			if ( confirmed ) {
-				if ( $link.get(0).form ) {
-					$( $link.get(0).form ).submit();
-				} else {
-					document.location = $link.attr('href');
+		var $message = $( "<div class=\"form-group\"><label>" + $link.data( "confirmationPrompt" ) + "</label></div>" )
+		  , $input   = $( "<input class=\"bootbox-input form-control\" autocomplete=\"off\" type=\"text\" />" )
+		  , match    = ""
+		;
+
+		if( typeof $link.data( "confirmationMatch" ) !== "undefined" ) {
+			match =  $link.data( "confirmationMatch" );
+
+			$message
+				.append( "<p class=\"help-block\">Please type <code>" + match + "</code> to proceed.</p>" )
+				.append( $input )
+			;
+		}
+
+		presideBootbox.dialog( {
+			  title   : "Confirmation"
+			, message : $message
+			, buttons : {
+				  cancel  : {
+				  	label: 'Cancel'
+				  }
+				, confirm : {
+					  label: 'Confirm'
+					, callback: function() {
+						var confirmed = false;
+
+						if( match.length ) {
+							if( $input.val() === match ) {
+								confirmed = true;
+								$message.removeClass( "has-error" );
+							}
+							else {
+								$message.addClass( "has-error" );
+							}
+						}
+						else {
+							confirmed = true;
+						}
+
+						if( confirmed ) {
+							if ( $link.get( 0 ).form ) {
+								$( $link.get( 0 ).form ).submit();
+							}
+							else {
+								document.location = $link.attr( "href" );
+							}
+						}
+
+						return false;
+					}
 				}
 			}
-		});
+		} );
+
 	});
 
 } )( presideJQuery );
