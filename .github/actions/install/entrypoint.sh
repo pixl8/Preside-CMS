41a630b14092deedf5cbd32f6c33ae090fbc31df
Dominic Watson <watson.dominic@gmail.com>
2020-10-29 13:18:23 +0000

    Use grunt from our base image to do grunt install

--- .github/actions/install/entrypoint.sh
+++ .github/actions/install/entrypoint.sh
@@ -6,3 +6,9 @@ box install --force save=false || exit 1;
 rm -rf ./system/externals/lucee-spreadsheet/javaLoader;
 rm -rf ./system/externals/lucee-spreadsheet/test;
 rm -rf ./system/externals/cfconcurrent/javaloader;
+
+cd ./system/assets
+npm install || exit 1
+grunt all || exit 1
+
+cd $GITHUB_WORKSPACE
