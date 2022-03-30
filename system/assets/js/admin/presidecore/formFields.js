5e72c4690c2bc0aa76cd150ff76c29ea4258ee6b
Teon Ooi <teon@pixl8.co.uk>
2022-03-30 18:25:38 +0800

    PRESIDECMS-2302 Update to replace repeated characters with delimiter.

--- system/assets/js/admin/presidecore/formFields.js
+++ system/assets/js/admin/presidecore/formFields.js
@@ -6,19 +6,20 @@
     $(".image-dimension-picker").imageDimensionPicker();
 
     $(".auto-slug").each( function(){
-        var $this = $(this)
-          , $basedOn = $this.parents("form:first").find("[name='" + $this.data( 'basedOn' ) + "']")
+        var $this         = $(this)
+          , $basedOn      = $this.parents("form:first").find("[name='" + $this.data( 'basedOn' ) + "']")
           , slugDelimiter = $this.data( "slugDelimiter" )
-          , startRegex = new RegExp( "^"+slugDelimiter, "g" )
-          , endRegex   = new RegExp( slugDelimiter+"$", "g" )
+          , repeatRegex   = new RegExp( slugDelimiter+"+", "g" )
+          , startRegex    = new RegExp( "^"+slugDelimiter, "g" )
+          , endRegex      = new RegExp( slugDelimiter+"$", "g" )
         ;
 
         $basedOn.keyup( function(e){
             var slug = $basedOn.val()
-                .replace( /\W/g, slugDelimiter )
-                .replace( /-+/g, slugDelimiter )
-                .replace( startRegex, "" )
-                .replace( endRegex  , "" )
+                .replace( /\W/g      , slugDelimiter )
+                .replace( repeatRegex, slugDelimiter )
+                .replace( startRegex , "" )
+                .replace( endRegex   , "" )
                 .toLowerCase();
 
             $this.val( slug ).trigger( "keyup" );
