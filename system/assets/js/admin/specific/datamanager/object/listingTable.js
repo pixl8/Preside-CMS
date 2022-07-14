6366e9cee9e1286f02b6b8db8630e74df57418ab
Teon Ooi <teon@pixl8.co.uk>
2022-07-14 19:18:00 +0800

    PRESIDECMS-2358 Fix datatables pagination to reset when filter.

--- system/assets/js/admin/specific/datamanager/object/listingTable.js
+++ system/assets/js/admin/specific/datamanager/object/listingTable.js
@@ -491,7 +491,11 @@
 					if ( filterState.favourites && filterState.favourites.length ) {
 						setFavourites( filterState.favourites );
 					}
+				} else {
+					filtersPopulated = true;
 				}
+
+
 			};
 
 			updateFilterFolderCount = function( $group ) {
@@ -708,8 +712,6 @@
 			};
 
 			prePopulateFilter = function( filter ) {
-				var loaded = false;
-
 				if ( filter && filter.length ) {
 					$( document ).on( "conditionBuilderInitialized", function(){
 						filtersPopulated = true;
