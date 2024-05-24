6941c49eeced2310fbcc2fdd73f87eebcb9a4769
Dominic Watson <watson.dominic@gmail.com>
2024-05-24 17:10:06 +0100

    Some basic site tree testing

---
+++ tests/e2e/tests/cypress/e2e/02.basic.admin.functionality/sitetree.cy.js
@@ -0,0 +1,38 @@
+describe('The sitetree admin', () => {
+  beforeEach(() => {
+    cy.superuserAdminLogin()
+  })
+
+  it('Should be accessible immediately on login and have various UI features we can view and use immediately (basic scan of the feature)', () => {
+    cy.get( '.page-header h1' ).should( 'contain.text', 'Default site' );
+    cy.get( '.nav-tabs a[href="#tab-sitetree"]' ).should( 'contain.text', 'Site tree' );
+    cy.get( '.nav-tabs a[href="#tab-page"]' ).should( 'contain.text', 'Grid view' );
+
+    cy.get( 'table.tree-table a.page-title' ).should( 'contain.text', 'Homepage' );
+    cy.get( 'table.tree-table a.page-title' ).should( 'contain.text', 'Login' );
+    cy.get( 'table.tree-table a.page-title' ).should( 'contain.text', '401 Access denied' );
+    cy.get( 'table.tree-table a.page-title' ).should( 'contain.text', '404 Not found' );
+
+    cy.get( 'table.tree-table i.fa.fa-caret-right.tree-toggler' ).click();
+
+    cy.get( 'table.tree-table tr.depth-2 a.page-title' ).should( 'contain.text', 'Forgotten password' );
+    cy.get( 'table.tree-table tr.depth-2 a.page-title' ).should( 'contain.text', 'Reset password' );
+  });
+
+  it('Should allow editing of pages', () => {
+    cy.get( 'table.tree-table a.page-title' ).contains( 'Homepage' ).click();
+
+    cy.get( 'textarea#teaser' ).should( 'be.visible' );
+    cy.wait( 500 );
+    cy.get( 'textarea#teaser' ).type( 'This is a teaser' );
+    cy.get( 'button[value=publish]' ).click();
+
+    cy.get( '.gritter-success' ).should( 'contain.text', 'Page saved successfully' )
+
+    cy.get( 'table.tree-table a.page-title' ).contains( 'Homepage' ).click();
+    cy.get( 'textarea#teaser' ).should( 'be.visible' ).should( 'have.value', 'This is a teaser' );
+    cy.wait( 500 );
+    cy.get( 'textarea#teaser' ).clear();
+    cy.get( 'button[value=publish]' ).click();
+  });
+});
--- tests/e2e/tests/cypress/support/commands.js
+++ tests/e2e/tests/cypress/support/commands.js
@@ -3,7 +3,7 @@ Cypress.Commands.add( 'superuserAdminLogin', () => {
 
 	cy.get( 'body' ).then( ( $body ) => {
 		if ( $body.text().includes( 'First time setup' ) ) {
-			 cy.get( 'input[ name=email_address ]' ).should( 'be.visible' ).type( Cypress.env( 'ADMIN_SUPERUSER_EMAIL' ) );
+			cy.get( 'input[ name=email_address ]' ).should( 'be.visible' ).type( Cypress.env( 'ADMIN_SUPERUSER_EMAIL' ) );
 		    cy.get( 'input[ name=password ]' ).should( 'be.visible' ).type( Cypress.env( 'ADMIN_SUPERUSER_PASSWORD' ) );
 		    cy.get( 'input[ name=passwordConfirmation ]' ).should( 'be.visible' ).type( Cypress.env( 'ADMIN_SUPERUSER_PASSWORD' ) );
 		    cy.get( 'button.btn.btn-danger' ).should( 'be.visible' ).should( 'contain.text', 'Setup user' ).click();
