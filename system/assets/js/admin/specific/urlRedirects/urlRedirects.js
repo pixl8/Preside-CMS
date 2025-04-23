aac06e1e0c3813d8ee1284f21b4bec564cc90a53
Teon Ooi <teon@pixl8.co.uk>
2025-04-23 17:32:29 +0800

    PRESIDECMS-3021 Update to reuse validator for custom warning message.

--- system/assets/js/admin/specific/urlRedirects/urlRedirects.js
+++ system/assets/js/admin/specific/urlRedirects/urlRedirects.js
@@ -3,30 +3,36 @@
 	$( '[name="redirect_type"]' ).on( 'change', function( e ) {
 		var $this      = $( this )
 		  , $formGroup = $this.closest( '.form-group' )
+		  , $form      = $this.closest( 'form' )
+		  , $validator = $form.validate()
 		;
 
-		$formGroup.removeClass( "has-warning has-error" );
-		$( '.help-block', $formGroup ).remove();
+		$formGroup.removeClass( "has-warning" );
 
 		if ( $this.val() != "302" ) {
 			$formGroup.addClass( "has-warning" );
 
-			$( '.clearfix', $formGroup ).after( '<div class="help-block">' + cfrequest.redirectType301Warning + '</div>' );
+			$validator.showErrors( {
+				[ "redirect_type" ]: cfrequest.redirectType301Warning
+			} );
 		}
 	} ).trigger( 'change' );
 
 	$( '[name="exact_match_only"]' ).on( 'change', function( e ) {
 		var $this      = $( this )
 		  , $formGroup = $this.closest( '.form-group' )
+		  , $form      = $this.closest( 'form' )
+		  , $validator = $form.validate()
 		;
 
 		$formGroup.removeClass( "has-warning" );
-		$( '.help-block', $formGroup ).remove();
 
 		if ( !$this.is( ":checked" ) ) {
 			$formGroup.addClass( "has-warning" );
 
-			$( '.clearfix', $formGroup ).after( '<div class="help-block">' + cfrequest.exactMatchOnlyfalseWarning + '</div>' );
+			$validator.showErrors( {
+				[ "redirect_type" ]: cfrequest.exactMatchOnlyfalseWarning
+			} );
 		}
 	} ).trigger( 'change' );
 
